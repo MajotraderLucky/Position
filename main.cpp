c1be92cc3f2c676fa346840d534a064afefc2c60
Sergey_Ryazanov <majo78@mail.ru>
2021-09-13 23:44:33 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+class Position {
+private:
+    int open;
+    int sLoss;
+    int tProfit;
+public:
+risk() {
+
+}
+};
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
