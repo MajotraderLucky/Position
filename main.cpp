590696e4c4fb30fb1e7c5a34dc5cfba581f56571
Sergey_Ryazanov <majo78@mail.ru>
2021-09-14 10:02:56 +0300

    New code add.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,22 @@ private:
     int sLoss;
     int tProfit;
 public:
-risk() {
-
-}
+int risk(int open, int sLoss) {
+    return sLoss - open;
+    }
+    void setPosition(int position_open, int position_sLoss)
+    {
+    open = position_open;
+    sLoss = position_sLoss;
+    }
 };
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int open = 11;
+    int sLoss = 5;
+
+    Position newLong;
+    newLong.setPosition(int position_open, int position_sLoss);
+    newLong.risk(int open, int sLoss);
+    std::cout << newLong.risk(int open, int sLoss);
     return 0;
 }
