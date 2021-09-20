3132e8b3c59faba3fcd8662ac2360e0f8e4f411e
Sergey_Ryazanov <majo78@mail.ru>
2021-09-20 22:25:27 +0300

    Change file MaxRisk.cpp

--- MaxRisk.cpp
+++ MaxRisk.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 
 int main() {
-
+std::cout << "Введите допустимый риск по позиции\n";
+std::cout << "---> ";
+double risk;
+std::cin >> risk;
+std::cout << "Введите цену открытия позиции\n";
+std::cout << "---> ";
+double pOpen;
+std::cin >> pOpen;
+std::cout << "Введите цену stop loss\n";
+std::cout << "---> ";
+double sLoss;
+std::cin >> sLoss;
+//Сначала выясним риск в одном лоте
+double oneLotRisk = pOpen - sLoss;
+//Сколько в допустимом риске рисков одного лота
+double size;
+        size = risk / oneLotRisk;
+        std::cout << "Размер лота позиции = " << size << "\n";
 }
