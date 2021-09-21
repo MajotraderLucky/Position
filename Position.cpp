1401cac80cd0308e5d5cbf3f65bbbb283ed5b213
Sergey_Ryazanov <majo78@mail.ru>
2021-09-21 18:59:12 +0300

    Change file Position.cpp

--- Position.cpp
+++ Position.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
 class Position {
 private:
+    double pRisk;
+    double pSize;
     double pOpen;
     double sLoss;
-    double tProfit;
-    double pVolume;
 public:
-double risk(double sLoss, double pOpen, double pVolume) {
-    return (sLoss - pOpen) * pVolume;
+double maxRisk(double sLoss, double pOpen, double pRisk) {
+    return pRisk / (pOpen - sLoss);
     }
-    double setPosition(double position_pOpen, double position_sLoss, double position_pVolume)
+    double setPosition(double position_pOpen, double position_sLoss, double position_pRisk)
     {
     pOpen   = position_pOpen;
     sLoss   = position_sLoss;
-    pVolume = position_pVolume;
+    pRisk = position_pRisk;
     }
 };
 int main() {
     //Расчёт риска по позиции
     Position newLong;
+    double position_pRisk = 0.0;
+    std::cout << "Введите максимальный риск по позиции\n ---> ";
+    std::cin >> position_pRisk;
     double position_pOpen = 0.0;
     std::cout << "Введите цену открытия позиции\n ---> ";
     std::cin >> position_pOpen;
     double position_sLoss = 0.0;
     std::cout << "Введите цену stop loss\n ---> ";
     std::cin >> position_sLoss;
-    double position_pVolume;
-    std::cout << "Введите объём позиции\n ---> ";
-    std::cin >> position_pVolume;
-    newLong.setPosition(position_sLoss, position_pOpen, position_pVolume);
-    std::cout << "По достижении уровня цены " << position_sLoss << " убыток позиции составит " << newLong.risk(position_sLoss, position_pOpen, position_pVolume);
+    newLong.setPosition(position_sLoss, position_pOpen, position_pRisk);
+    std::cout << "Рабочий объём по позиции = " << newLong.maxRisk(position_sLoss, position_pOpen, position_pRisk);
     return 0;
 }
